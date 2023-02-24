7cec9e61fcb43eebbfa0dc242cb6c8f9536b4c8d
Divya Preetha Aravindan <jesusdiv18@gmail.com>
2023-02-24 22:24:58 +0530

    Create 6.FloatArithmetic.java

---
+++ 6.FloatArithmetic.java
@@ -0,0 +1,20 @@
+//Java program to check basic arithmetic operations like +,-,*,/,% on floating point numbers
+public class FloatArithmetic {
+    //Initialize the main() method
+    public static void main(String[] args) {
+        
+        //Initialize three float values to perform relational operations
+        float a = 20.5F, b = 6.4F;
+
+        //Print the tw input values
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        //Perform the relational operations and print the result
+        System.out.println("a+b = " + (a+b));
+        System.out.println("a-b = " + (a-b));
+        System.out.println("a*b = " + (a*b));
+        System.out.println("a/b = " + (a/b));
+        System.out.println("a%b = " + (a%b));
+    }
+}
