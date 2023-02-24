2dc1999d8509f54dd694ddbc24334e8ee6a507a8
Divya Preetha Aravindan <jesusdiv18@gmail.com>
2023-02-24 10:36:11 +0530

    Create SquareRoot.java

---
+++ SquareRoot.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+public class SquareRoot {
+	public static void main(String[] args) {
+		double a = 25;
+		double s = Math.sqrt(a);
+		System.out.println("The square root of " + a + " is " + s);
+	}
+}
