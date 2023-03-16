bdb694740aac85bb46de535d89a11708f0bb646e
Divya Preetha Aravindan <jesusdiv18@gmail.com>
2023-03-16 14:34:15 +0530

    Create IPS7-DebugJavaExpression.java

---
+++ IPS7-DebugJavaExpression.java
@@ -0,0 +1,11 @@
+//Debugging Question
+import java.util.*;
+public class Float {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        int ans = (2 + (5 -8) + (5 -5) + 10 ) * 2 + x;
+        System.out.println(ans);
+        scan.close();
+    }
+}
