0ba41a04f0d08ecf0ba171cf5a00e104bd80f4a2
Divya Preetha Aravindan <jesusdiv18@gmail.com>
2023-03-16 20:31:04 +0530

    Update IPS7-DebugJavaExpression.java

--- IPS7-DebugJavaExpression.java
+++ IPS7-DebugJavaExpression.java
@@ -1,4 +1,26 @@
 //Debugging Question
+/*
+Debug the errors in the following code snippet
+
+import java.util.*;
+class Main {
+public static void main(String[] args) {
+Scanner scan = new Scanner(System.in);
+int x = scan.nextInt();
+int ans = (2 (+5 -8) (+5 -5) + 10) * 2 + x;
+System.out.println(ans);
+}
+}
+
+
+Test Case
+Input Format
+1
+Output Format
+19
+
+
+*/
 import java.util.*;
 public class Float {
     public static void main(String[] args) {
