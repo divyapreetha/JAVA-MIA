12c9ba1a8f47188511cd7e958886be654f45a39c
Divya Preetha Aravindan <jesusdiv18@gmail.com>
2023-04-22 22:40:54 +0530

    Create Leetcode1929-ArrayConcatenation.java

---
+++ Leetcode1929-ArrayConcatenation.java
@@ -0,0 +1,23 @@
+/*
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+
+*/
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int ans[] = new int[2*n];
+        for(int i = 0; i<ans.length; i++) {
+            if(i<n){
+                ans[i] = nums[i];
+            }
+            else {
+                ans[i] = nums[i-n];
+            }
+        }
+        return ans;
+    }
+}
